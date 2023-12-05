061507256aaa5076ba251efaf483eefcbb0a8991
Kael Ding <415676243@qq.com>
2023-12-05 15:01:04 +0800

    Update RN 0.68.5 to 0.72.3

--- example/metro.config.js
+++ example/metro.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const uikitPak = require('../../../package.json');
-const prebuiltPak = require('../package.json')
+const prebuiltPak = require('../package.json');
 
 const prebuiltRoot = path.resolve(__dirname, '..');
-const uikitRoot = path.resolve(__dirname, '../../..')
+const uikitRoot = path.resolve(__dirname, '../../..');
 
 const uikitModules = Object.keys({
   ...uikitPak.peerDependencies,
@@ -22,19 +22,22 @@ module.exports = {
   // We need to make sure that only one version is loaded for peerDependencies
   // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: exclusionList(
-      [...uikitModules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(uikitRoot, 'node_modules', m))}\\/.*$`)
+    blacklistRE: exclusionList([
+      ...uikitModules.map(
+        m =>
+          new RegExp(
+            `^${escape(path.join(uikitRoot, 'node_modules', m))}\\/.*$`,
+          ),
       ),
       ...prebuiltModules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(prebuiltRoot, 'node_modules', m))}\\/.*$`)
-      )
-      ]
-    ),
+        m =>
+          new RegExp(
+            `^${escape(path.join(prebuiltRoot, 'node_modules', m))}\\/.*$`,
+          ),
+      ),
+    ]),
 
-    extraNodeModules: uikitModules.reduce((acc, name) => {
+    extraNodeModules: [...uikitModules, ...prebuiltModules].reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
     }, {}),
--- example/src/KeyCenter.ts
+++ example/src/KeyCenter.ts
@@ -1,6 +1,6 @@
 const KeyCenter = {
-  appID: 252984006,
-  appSign: '7328c93dfd9c7cf300818cde931c661b545bfc098fce80c722fdbd8d1a3b262a',
+  appID: ,
+  appSign: '',
 };
 
 export default KeyCenter;
